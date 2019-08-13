19b4668e6f836bd08c0c9a8fb31f2f47ba13654c
Brandi <brandiclinard021911@gmail.com>
2019-08-13 16:08:48 -0500

    started exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,12 @@
 USE codeup_test_db;
 
-INSERT INTO albums()
-VALUES ();
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES   ('Michael Jackson', 'Thriller', '1982', '47.3', 'Pop, Rock, R&B'),
+                ('Eagles', 'Their Greatest Hits (1971-1975)', '1976', '41.2', 'Country Rock, Soft Rock, Folk Rock'),
+                ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard Rock'),
+                ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive Rock'),
+                ('Whitney Houston', 'The Bodyguard', '1992','28.4', 'R&B, Soul, Pop, Soundtrack'),
+                ('Meat Loaf', 'Bat Out of Hell', '1977', '21.5', 'Hard Rock, Glam Rock, Heavy Metal'),
+                ('Eagles', 'Hotel california', '1976', '31.5', 'Soft Rock'),
+                ('Bee Gees', 'Saturday Night Fever', '1977', '21.6', 'Disco'),
+                ('Fleetwood Mac', 'Rumours', '1977', '27.9', 'Soft Rock'),
