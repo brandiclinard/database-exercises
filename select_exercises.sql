60e4f42db76eddea762a7e2bf2ab96c16b71cea2
Brandi <brandiclinard021911@gmail.com>
2019-08-14 09:47:38 -0500

    All input for albulms in place. started selecting info

---
+++ select_exercises.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+SELECT 'All albums by Pink Floyd:' AS 'Info';
+
+
+
+SELECT "The year Sgt. Pepper's Lonely Hearts Club Band was released:" AS 'Info';
+
+SELECT 'The genre for Nevermind:' AS 'Info';
+
+SELECT 'Albums released in the 1990s:' AS 'Info';
+
+SELECT 'Albums with less than 20 million certified sales' AS 'Info';
+
+SELECT 'All albums with a genre of "Rock":' AS 'Info';
