37ffb8e232673508cb715059aaa8b5bef880ebcf
Brandi <brandiclinard021911@gmail.com>
2019-08-14 11:31:32 -0500

    update exercise complete

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,36 +1,36 @@
 USE codeup_test_db;
 
-SELECT 'All albums by Pink Floyd:' AS 'Info';
+SELECT 'All albums by Pink Floyd:' AS 'Caption';
 
 SELECT name
 FROM albums
 WHERE artist = 'Pink Floyd';
 
-SELECT "The year Sgt. Pepper's Lonely Hearts Club Band was released:" AS 'Info';
+SELECT "The year Sgt. Pepper's Lonely Hearts Club Band was released:" AS 'Caption';
 
 SELECT release_date
 FROM albums
 WHERE name = "Sgt. Pepper's Lonely Hearts Club Band";
 
-SELECT 'The genre for Nevermind:' AS 'Info';
+SELECT 'The genre for Nevermind:' AS 'Caption';
 
 SELECT genre
 FROM albums
 WHERE name = 'Nevermind';
 
-SELECT 'Albums released in the 1990s:' AS 'Info';
+SELECT 'Albums released in the 1990s:' AS 'Caption';
 
 SELECT name
 FROM albums
 WHERE release_date  BETWEEN 1900 AND 2000;
 
-SELECT 'Albums with less than 20 million certified sales' AS 'Info';
+SELECT 'Albums with less than 20 million certified sales' AS 'Caption';
 
 SELECT name
 FROM albums
 WHERE sales < 20;
 
-SELECT 'All albums with a genre of "Rock":' AS 'Info';
+SELECT 'All albums with a genre of "Rock":' AS 'Caption';
 
 SELECT name
 FROM albums
---
+++ update_exercises.sql
@@ -0,0 +1,48 @@
+USE codeup_test_db;
+
+SELECT 'All albums:' AS 'Caption';
+
+SELECT name
+FROM albums;
+
+UPDATE albums
+SET sales = sales * 10
+WHERE sales IS NOT NULL;
+
+SELECT 'Updated album sales:' AS 'Caption';
+
+SELECT name, sales
+FROM albums\G
+
+SELECT 'All albums released before 1980:' AS 'Caption';
+
+SELECT name
+FROM albums
+WHERE release_date > 1980;
+
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+SELECT 'Updated release dates:' AS 'Caption';
+
+SELECT name, release_date
+FROM albums\G
+
+SELECT 'All albums by Michael Jackson:' AS 'Caption';
+
+SELECT name
+FROM albums
+WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET artist = "Peter Jackson"
+WHERE artist = "Michael Jackson";
+
+SELECT name, album
+FROM albums
+WHERE artist = "Peter Jackson";
+
+
+
+
