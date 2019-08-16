633eb5e0d7a9332b52fa3322dafa705d61660a04
Brandi <brandiclinard021911@gmail.com>
2019-08-16 09:41:26 -0500

    indexes exercises

---
+++ indexes_exercises.sql
@@ -0,0 +1,8 @@
+Use employees;
+
+describe departments; /*primary key: dept_no-- unqiue key: dept_name*/
+describe dept_emp;/*primary key: emp_no, dept_no*/
+describe dept_manager;/*primary key: dept_no, emp_no*/
+describe employees;/*primary key: emp_no*/
+describe salaries;/*primary key: emp_no, from_date*/
+describe titles;/*primary key: emp_no, title, from_date*/
